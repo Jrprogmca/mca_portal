c34f408f32fdd0195d007e432b04b56106afa0c6
Jrprogmca <97666892+Jrprogmca@users.noreply.github.com>
2023-03-14 15:27:20 +0530

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Akshay Shirodkar
+|--------------------------------------------------------------------------
+| Author: Akshay Shirodkar - Jr. Programmer
+| Project Name: MCA Portal
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'akshay' );             // Set database user
+  define( 'DB_PASS', 'nopass' );             // Set database password
+  define( 'DB_NAME', 'mcaportal' );         // Set database name
+
+?>
